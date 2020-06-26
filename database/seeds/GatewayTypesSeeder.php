d9db2b9bc57afd9d0b26be58130a9036f3f3f08c
Kendall Arneaud <kendall.arneaud@gmail.com>
2020-06-26 17:09:34 -0400

    Update Gateways

--- database/seeds/GatewayTypesSeeder.php
+++ database/seeds/GatewayTypesSeeder.php
@@ -8,23 +8,10 @@ class GatewayTypesSeeder extends Seeder
     {
         Eloquent::unguard();
 
-        // fix for legacy data
-        DB::statement('UPDATE gateway_types SET alias = "custom1" WHERE id = 6');
-
         $gateway_types = [
             ['alias' => 'credit_card', 'name' => 'Credit Card'],
             ['alias' => 'bank_transfer', 'name' => 'Bank Transfer'],
             ['alias' => 'paypal', 'name' => 'PayPal'],
-            ['alias' => 'bitcoin', 'name' => 'Bitcoin'],
-            ['alias' => 'dwolla', 'name' => 'Dwolla'],
-            ['alias' => 'custom1', 'name' => 'Custom'],
-            ['alias' => 'alipay', 'name' => 'Alipay'],
-            ['alias' => 'sofort', 'name' => 'Sofort'],
-            ['alias' => 'sepa', 'name' => 'SEPA'],
-            ['alias' => 'gocardless', 'name' => 'GoCardless'],
-            ['alias' => 'apple_pay', 'name' => 'Apple Pay'],
-            ['alias' => 'custom2', 'name' => 'Custom'],
-            ['alias' => 'custom3', 'name' => 'Custom'],
         ];
 
         foreach ($gateway_types as $gateway_type) {
